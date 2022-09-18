8934ac875a0938b58c694b2ff2b01d18574beace
AnastasiiaFaleeva <113059067+AnastasiiaFaleeva@users.noreply.github.com>
2022-09-18 15:50:34 +0300

    add task011

---
+++ exem011/Program.cs
@@ -0,0 +1,56 @@
+﻿// МЕТОД (ФУНКЦИЯ ЗАПОЛНЕНИЯ МАССИВА)
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+// МЕТОД (ФУНКЦИЯ) ВЫВОДА МАССИВА НА ЭКРАН
+void PrintArray(int[] col)     // это метод (функция) которая ничего не возвращает
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; // указали, что по умочанию в данном массиве находитс 10 элементов
+                           // СОЗДАЙ массив в котором будет 10 элементов
+
+FillArray(array);
+array[4] = 4;
+array[6] = 4;
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 444);
+Console.WriteLine(pos);
+
